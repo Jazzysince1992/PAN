86aa99fd956942f612f66ad63086cc27d8693201
Jazzysince1992 <Saurabhjaiswaljazzy@gmail.com>
2023-08-23 14:40:33 +0700

    Python basics 23/08/23

--- Python Basics/Hello World.py
+++ Python Basics/Hello World.py
@@ -2,3 +2,28 @@ person = "Jazzy"
 print("Hello",person)
 person ="Ruchika"
 print("Hello",person)
+sno =1200
+flag = True
+fno = 2.3
+print(type(person))
+print(type(sno))
+print(type(flag))
+print(type(fno))
+print("Hello",person,str(sno))
+num1 =199
+num2=100
+print(num1+num2)
+if num1>num2:
+    print("First number is greater than second number")
+
+
+fruit = ["Mango", "Banana", "Grapes","Kiwi"]
+print(type(fruit))
+print(fruit)
+print(fruit[2])
+# . Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
+names = ["Jazzy","Saurabh","Elon","Musk"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
